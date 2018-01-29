2f63901dc6ae12642ca9b2cbd70eed6b95c80f77
Ryan Oldford <ryan.oldford@gmail.com>
2018-01-29 13:30:13 +0800

    Updated app to use Sidebar and BuildingSelector components, developed framework for Sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import Header from './components/Header';
-import ScoringInput from './components/ScoringInput';
+import BuildingSelector from './components/BuildingSelector';
+import Sidebar from './components/Sidebar';
+// import ScoringInput from './components/ScoringInput';
 
 
 class App extends Component {
@@ -14,9 +16,16 @@ class App extends Component {
               <Header />
             </Col>
           </Row>
-            <Col>
-              <ScoringInput />
-            </Col>
+          <Row>
+            {
+              // Sidebar with scorebox and place to enter VP chip count
+            }
+            <Col sm="2"><Sidebar/></Col>
+            {
+              // Building Selection Area
+            }
+            <Col sm="10"><BuildingSelector /></Col>
+          </Row>
         </Container>
       </div>
     );
---
+++ src/components/BuildingSelector.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Card, CardSubtitle, CardTitle} from 'reactstrap';
+
+const BuildingSelector = () => (
+  <Card body className="text-center">
+    <CardTitle>
+      BuildingSelector test card
+    </CardTitle>
+    <CardSubtitle>
+      This is a test card for the BuildingSelector.
+    </CardSubtitle>
+  </Card>
+);
+
+export default BuildingSelector;
