bc6a4b62dcc2913dfae92cdba9a6184b3b252b5a
Ryan Oldford <ryan.oldford@gmail.com>
2018-01-29 16:32:14 +0800

    Renamed Sidebar to Scoring, edited to give better layout and styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {Container, Row, Col} from 'reactstrap';
 import Header from './components/Header';
 import BuildingSelector from './components/BuildingSelector';
-import Sidebar from './components/Sidebar';
+import Scoring from './components/Scoring';
 // import ScoringInput from './components/ScoringInput';
 
 
@@ -17,14 +17,10 @@ class App extends Component {
             </Col>
           </Row>
           <Row>
-            {
-              // Sidebar with scorebox and place to enter VP chip count
-            }
-            <Col sm="2"><Sidebar/></Col>
-            {
-              // Building Selection Area
-            }
-            <Col sm="10"><BuildingSelector /></Col>
+            <Col><Scoring /></Col>
+          </Row>
+          <Row>
+            <Col><BuildingSelector /></Col>
           </Row>
         </Container>
       </div>
---
+++ src/components/Scoring.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Badge,
+  Card, 
+  CardGroup, 
+  CardTitle, 
+  CardText, 
+  Col, 
+  Container, 
+  Input, 
+  InputGroup, 
+  Row
+} from 'reactstrap';
+
+const Scoring = () => (
+  <Container>
+    <Row>
+      <Col>
+        <Card className="text-center">
+          <CardText>
+            <p className="text-muted">Total Points</p>
+            <h2>49</h2>
+          </CardText>
+        </Card>
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <hr />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        <CardGroup>
+          <Card className="text-center">
+            <CardText>
+              <p className="text-muted">VP Chips</p>
+              <Row>
+                <Col className="text-right"><Badge href="#">+</Badge></Col>
+                <Col xs="auto"><h4>23</h4></Col>
+                <Col className="text-left"><Badge href="#">-</Badge></Col>
+              </Row>
+            </CardText>
+          </Card>
+          <Card className="text-center">
+            <CardText>
+              <p className="text-muted">Buildings</p>
+              <h4>49</h4>
+            </CardText>
+          </Card>
+          <Card className="text-center">
+            <CardText>
+              <p className="text-muted">Bonuses</p>
+              <h4>49</h4>
+            </CardText>
+          </Card>
+        </CardGroup>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Scoring;
