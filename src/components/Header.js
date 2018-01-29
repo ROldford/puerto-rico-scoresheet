22ac90e06fb6b345d8923e9ba547c3f162327ef6
Ryan Oldford <ryan.oldford@gmail.com>
2018-01-29 12:30:53 +0800

    Finished styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {Jumbotron, Card, CardSubtitle, CardTitle} from 'reactstrap';
+
+// const Header = () => (
+//   <Jumbotron>
+//     <h4 className="display-4">Puerto Rico Scoresheet</h4>
+//     <p className="lead">
+//       Helping you figure out how badly you lost since 2018
+//     </p>
+//   </Jumbotron>
+// );
 
 const Header = () => (
-  <h2>Puerto Rico Scoresheet</h2>
+  <Card body className="text-center">
+    <CardTitle>
+      <h4 className="display-4">Puerto Rico Scoresheet</h4>
+    </CardTitle>
+    <CardSubtitle>
+      <p className="lead">
+        Helping you figure out how badly you lost since 2018
+      </p>
+    </CardSubtitle>
+  </Card>
 );
 
 export default Header;
