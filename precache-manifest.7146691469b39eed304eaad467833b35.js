351d54bd07c61e17edeef710a11302f299c778e9
Ryan Oldford <ryan.oldford@gmail.com>
2018-11-02 10:02:32 +0800

    Updates

---
+++ precache-manifest.7146691469b39eed304eaad467833b35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a984ec36800d57d29168",
+    "url": "/puerto-rico-scoresheet/static/css/main.2a0a2f01.chunk.css"
+  },
+  {
+    "revision": "a984ec36800d57d29168",
+    "url": "/puerto-rico-scoresheet/static/js/main.a984ec36.chunk.js"
+  },
+  {
+    "revision": "5ab02389c47d3e6a8ed1",
+    "url": "/puerto-rico-scoresheet/static/css/1.6c066d6a.chunk.css"
+  },
+  {
+    "revision": "5ab02389c47d3e6a8ed1",
+    "url": "/puerto-rico-scoresheet/static/js/1.5ab02389.chunk.js"
+  },
+  {
+    "revision": "a7f7c6264790faf5b9c9",
+    "url": "/puerto-rico-scoresheet/static/js/runtime~main.a7f7c626.js"
+  },
+  {
+    "revision": "b3f900636c6eb6387c2391d864360156",
+    "url": "/puerto-rico-scoresheet/index.html"
+  }
+];
