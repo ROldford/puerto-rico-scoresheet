9e10b2afa59989036f9d9b7120f045a101f485ca
Ryan Oldford <ryan.oldford@gmail.com>
2019-05-03 12:31:45 +0800

    Updates

---
+++ precache-manifest.266bc09b357d0c4b35fa210b3577c80f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ecf35d1463afb64335fa27b0a509a04",
+    "url": "/puerto-rico-scoresheet/index.html"
+  },
+  {
+    "revision": "2447b931097a5185bf66",
+    "url": "/puerto-rico-scoresheet/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "5828c38a65fc45b5f74e",
+    "url": "/puerto-rico-scoresheet/static/css/main.7425a10f.chunk.css"
+  },
+  {
+    "revision": "2447b931097a5185bf66",
+    "url": "/puerto-rico-scoresheet/static/js/2.a45df3ef.chunk.js"
+  },
+  {
+    "revision": "5828c38a65fc45b5f74e",
+    "url": "/puerto-rico-scoresheet/static/js/main.9db9a1c7.chunk.js"
+  },
+  {
+    "revision": "382d482c13f783233a8d",
+    "url": "/puerto-rico-scoresheet/static/js/runtime~main.90f87cf2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/puerto-rico-scoresheet/precache-manifest.9526f00e01bfd43a536132ff10fbd4fe.js"
+  "/puerto-rico-scoresheet/precache-manifest.266bc09b357d0c4b35fa210b3577c80f.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/puerto-rico-scoresheet/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puerto-rico-scoresheet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
