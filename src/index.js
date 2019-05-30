e64c86dbd8a5f5d321a998dc14652ab4ddc94df8
Ryan Oldford <ryan.oldford@gmail.com>
2019-05-30 09:32:19 +0800

    Store is configured and accesible in app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import configureStore from './store/configureStore';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
