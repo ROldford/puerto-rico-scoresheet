94fa034650486a149839d2dcf97dc03dda99f686
Ryan Oldford <ryan.oldford@gmail.com>
2019-05-30 08:45:46 +0800

    Updated actions

--- src/actions/actionCreators.js
+++ src/actions/actionCreators.js
@@ -1,25 +1,21 @@
 import * as types from './actionTypes';
 
 export function addVpChips(amount) {
-  return { type: ADD_VP_CHIPS, amount }
+  return { type: types.ADD_VP_CHIPS, amount }
 }
 
 export function subtractVpChips(amount) {
-  return { type: SUBTRACT_VP_CHIPS, amount }
+  return { type: types.SUBTRACT_VP_CHIPS, amount }
 }
 
 export function toggleBuildingBuilt(buildingName) {
-  return { type: TOGGLE_BUILDING_BUILT, buildingName }
+  return { type: types.TOGGLE_BUILDING_BUILT, buildingName }
 }
 
 export function toggleBuildingHasWorker(buildingName) {
-  return { type: TOGGLE_BUILDING_HAS_WORKER, buildingName }
+  return { type: types.TOGGLE_BUILDING_HAS_WORKER, buildingName }
 }
 
-export function setIslandSpacesOccupied(amount) {
-  return { type: SET_ISLAND_SPACES_OCCUPIED, amount }
-}
-
-export function setTotalColonists(amount) {
-  return { type: SET_TOTAL_COLONISTS, amount }
+export function setBonusInput(inputValue) {
+  return { type: types.SET_BONUS_INPUT, inputValue }
 }
